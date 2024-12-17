0a9cf213fa62c488a6410a19aea9b75d84463ca4
Max <85294320+Maxthecoder123@users.noreply.github.com>
2024-12-17 17:09:02 +0000

    E15 - Basic Save & Load

--- Levels/scripts/level.gd
+++ Levels/scripts/level.gd
@@ -5,3 +5,9 @@ class_name Level extends Node2D
 func _ready():
 	self.y_sort_enabled = true
 	PlayerManager.set_as_parent(self)
+	LevelManager.level_loat_started.connect(_free_level)
+
+
+func _free_level() -> void:
+	PlayerManager.unparent_player(self)
+	queue_free()
